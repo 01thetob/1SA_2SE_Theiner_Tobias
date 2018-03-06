dbf157336eb55ab14cbac1ec3e8d8063b7ca1d35
Schularbeit16 <Schularbeit16@rg-me.schule.suedtirol.it>
2018-03-06 11:51:28 +0100

    erstellung der Lables+Textboxes

--- Auto/Auto/Form1.Designer.cs
+++ Auto/Auto/Form1.Designer.cs
@@ -28,12 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Marke = new System.Windows.Forms.TextBox();
+            this.PS = new System.Windows.Forms.TextBox();
+            this.Tankgroeße = new System.Windows.Forms.TextBox();
+            this.Verbrauch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Neues Auto erstellen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marke:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Farbe:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ps:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Verbrauch:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(189, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tankgröße:";
+            // 
+            // Marke
+            // 
+            this.Marke.Location = new System.Drawing.Point(58, 50);
+            this.Marke.Name = "Marke";
+            this.Marke.Size = new System.Drawing.Size(100, 20);
+            this.Marke.TabIndex = 6;
+            // 
+            // PS
+            // 
+            this.PS.Location = new System.Drawing.Point(58, 103);
+            this.PS.Name = "PS";
+            this.PS.Size = new System.Drawing.Size(100, 20);
+            this.PS.TabIndex = 7;
+            // 
+            // Tankgroeße
+            // 
+            this.Tankgroeße.Location = new System.Drawing.Point(254, 50);
+            this.Tankgroeße.Name = "Tankgroeße";
+            this.Tankgroeße.Size = new System.Drawing.Size(100, 20);
+            this.Tankgroeße.TabIndex = 8;
+            // 
+            // Verbrauch
+            // 
+            this.Verbrauch.Location = new System.Drawing.Point(254, 76);
+            this.Verbrauch.Name = "Verbrauch";
+            this.Verbrauch.Size = new System.Drawing.Size(100, 20);
+            this.Verbrauch.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 262);
+            this.Controls.Add(this.Verbrauch);
+            this.Controls.Add(this.Tankgroeße);
+            this.Controls.Add(this.PS);
+            this.Controls.Add(this.Marke);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Marke;
+        private System.Windows.Forms.TextBox PS;
+        private System.Windows.Forms.TextBox Tankgroeße;
+        private System.Windows.Forms.TextBox Verbrauch;
     }
 }
 
